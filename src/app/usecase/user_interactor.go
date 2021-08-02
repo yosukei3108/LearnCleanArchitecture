cea1bfd6e5396414c2d373035d43f86fc8b7bd09
yosukei3108 <38313011+yosukei3108@users.noreply.github.com>
2021-08-02 09:06:39 +0900

    add usecase dir as Use cases layer

---
+++ src/app/usecase/user_interactor.go
@@ -0,0 +1,22 @@
+package usecase
+
+import "app/domain"
+
+type UserInteractor struct {
+	UserRepository UserRepository
+}
+
+func (interactor *UserInteractor) Add(u domain.User) (err error) {
+	_, err := interactor.UserRepository.Store()
+	return
+}
+
+func (interactor *UserInteractor) Users(user domain.Users) (err error) {
+	user, err := interactor.UserRepository.FindAll()
+	return
+}
+
+func (interactor *UserInteractor) UserById(identifier int) (u domain.User) (err error) {
+	user, err := interactor.UserRepository.FindById(identifier)
+	return
+}
