6a9c7d732b59bd0165e721ed8ae0940486050afc
yosukei3108 <38313011+yosukei3108@users.noreply.github.com>
2021-08-03 14:06:55 +0900

    fix syntax error

--- src/app/usecase/user_interactor.go
+++ src/app/usecase/user_interactor.go
@@ -7,16 +7,16 @@ type UserInteractor struct {
 }
 
 func (interactor *UserInteractor) Add(u domain.User) (err error) {
-	_, err := interactor.UserRepository.Store()
+	_, err = interactor.UserRepository.Store(u)
 	return
 }
 
 func (interactor *UserInteractor) Users() (user domain.Users, err error) {
-	user, err := interactor.UserRepository.FindAll()
+	user, err = interactor.UserRepository.FindAll()
 	return
 }
 
-func (interactor *UserInteractor) UserById(identifier int) (u domain.User, err error) {
-	user, err := interactor.UserRepository.FindById(identifier)
+func (interactor *UserInteractor) UserById(identifier int) (user domain.User, err error) {
+	user, err = interactor.UserRepository.FindById(identifier)
 	return
 }
